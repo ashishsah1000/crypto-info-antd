c8a3a55fa37cb270eb115d8e077c54d6afed6de1
Ashish sah <ashishsah1000@gmail.com>
2022-03-08 10:45:57 +0530

    Added the redux Toolkit used redux toolkit to get the first api data of coins. store and reducer has been set

---
+++ src/services/cryptoApi.js
@@ -0,0 +1,39 @@
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react/";
+
+const cryptoApiHeaders = {
+  "x-rapidapi-host": "coinranking1.p.rapidapi.com",
+  "x-rapidapi-key": "4d36d0690amsha9fb495c3428903p1e73a3jsnbdf92e7f3487"
+};
+
+const baseUrl = "https://coinranking1.p.rapidapi.com";
+
+const createRequest = (url) => ({ url, headers: cryptoApiHeaders });
+export const cryptoApi = createApi({
+  reducerPath: "cryptoApi",
+  baseQuery: fetchBaseQuery({ baseUrl }),
+  endpoints: (builder) => ({
+    getCryptos: builder.query({
+      query: () => createRequest("/coins")
+    })
+  })
+});
+
+export const { useGetCryptosQuery } = cryptoApi;
+
+// var options = {
+//   method: 'GET',
+//   url: 'https://coinranking1.p.rapidapi.com/coins',
+//   params: {
+//     referenceCurrencyUuid: 'yhjMzLPhuIDl',
+//     timePeriod: '24h',
+//     tiers: '1',
+//     orderBy: 'marketCap',
+//     orderDirection: 'desc',
+//     limit: '50',
+//     offset: '0'
+//   },
+//   headers: {
+//     'x-rapidapi-host': 'coinranking1.p.rapidapi.com',
+//     'x-rapidapi-key': '4d36d0690amsha9fb495c3428903p1e73a3jsnbdf92e7f3487'
+//   }
+// };
