999263c95c384e40c1efcace22600067a577a0cf
Ashish sah <ashishsah1000@gmail.com>
2022-03-10 08:55:35 +0530

    Created card of cryptos Error resides in the css as the card is not taking 100% of the desired width

--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -2,15 +2,16 @@ import React from "react";
 import millify from "millify";
 import { Typography, Row, Col, Statistic } from "antd";
 import { Link } from "react-router-dom";
+import { Cryptocurrencies, News } from "./index";
 
 import { useGetCryptosQuery } from "../services/cryptoApi";
 
 const { Title } = Typography;
 const Homepage = () => {
   const { data, isFetching } = useGetCryptosQuery();
-  const globalStats = data?.data?.stats
-  console.log(data)
-  if(isFetching) return "Loading... "
+  const globalStats = data?.data?.stats;
+  console.log(data);
+  if (isFetching) return "Loading... ";
   return (
     <>
       <Title level={2} className="heading">
@@ -18,21 +19,54 @@ const Homepage = () => {
       </Title>
       <Row>
         <Col span={12}>
-          <Statistic title="Total CryptoCurencies" value={millify(globalStats.total)} />
+          <Statistic
+            title="Total CryptoCurencies"
+            value={millify(globalStats.total)}
+          />
         </Col>
         <Col span={12}>
-          <Statistic title="Total Exchanges" value={millify(globalStats.totalExchanges)} />
+          <Statistic
+            title="Total Exchanges"
+            value={millify(globalStats.totalExchanges)}
+          />
         </Col>
         <Col span={12}>
-          <Statistic title="Total Market Cap" value={millify(globalStats.totalMarketCap)} />
+          <Statistic
+            title="Total Market Cap"
+            value={millify(globalStats.totalMarketCap)}
+          />
         </Col>
         <Col span={12}>
-          <Statistic title="Total 24h Volume" value={millify(globalStats.total24hVolume)} />
+          <Statistic
+            title="Total 24h Volume"
+            value={millify(globalStats.total24hVolume)}
+          />
         </Col>
         <Col span={12}>
-          <Statistic title="Total Markets" value={millify(globalStats.totalMarkets)} />
+          <Statistic
+            title="Total Markets"
+            value={millify(globalStats.totalMarkets)}
+          />
         </Col>
       </Row>
+      <div className="home-heading-container">
+        <Title className="home-title" level={2}>
+          Top 10 Cryptocurrencies in the world
+        </Title>
+        <Title className="show-more" level={3}>
+          <Link to="/cryptocurrencies">Show More</Link>
+        </Title>
+      </div>
+      <Cryptocurrencies simplified />
+      <div className="home-heading-container">
+        <Title className="home-title" level={2}>
+          Latest Cryptocurrencies news
+        </Title>
+        <Title className="show-more" level={3}>
+          <Link to="/cryptocurrencies">Show More</Link>
+        </Title>
+      </div>
+      <News simplified />
     </>
   );
 };
