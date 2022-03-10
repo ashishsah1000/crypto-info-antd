b174edfd27b0a72f0c8fa7c46c1fd5a9b8aba7c4
Ashish sah <ashishsah1000@gmail.com>
2022-03-10 08:13:10 +0530

    Added the status using redux Toolkit Created an api end request to /home and used millify to better express the data

--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -8,8 +8,9 @@ import { useGetCryptosQuery } from "../services/cryptoApi";
 const { Title } = Typography;
 const Homepage = () => {
   const { data, isFetching } = useGetCryptosQuery();
-
-  console.log(data);
+  const globalStats = data?.data?.stats
+  console.log(data)
+  if(isFetching) return "Loading... "
   return (
     <>
       <Title level={2} className="heading">
@@ -17,19 +18,19 @@ const Homepage = () => {
       </Title>
       <Row>
         <Col span={12}>
-          <Statistic title="Total CryptoCurencies" value={5} />
+          <Statistic title="Total CryptoCurencies" value={millify(globalStats.total)} />
         </Col>
         <Col span={12}>
-          <Statistic title="Total Exchanges" value={5} />
+          <Statistic title="Total Exchanges" value={millify(globalStats.totalExchanges)} />
         </Col>
         <Col span={12}>
-          <Statistic title="Total Market Cap" value={5} />
+          <Statistic title="Total Market Cap" value={millify(globalStats.totalMarketCap)} />
         </Col>
         <Col span={12}>
-          <Statistic title="Total 24h Volume" value={5} />
+          <Statistic title="Total 24h Volume" value={millify(globalStats.total24hVolume)} />
         </Col>
         <Col span={12}>
-          <Statistic title="Total Markets" value={5} />
+          <Statistic title="Total Markets" value={millify(globalStats.totalMarkets)} />
         </Col>
       </Row>
     </>
