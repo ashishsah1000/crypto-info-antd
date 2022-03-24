fa0b807500687c2c908d399fe4818541553e0ed4
Ashish sah <ashishsah1000@gmail.com>
2022-03-24 12:13:41 +0530

    Added News Api using redux toolkit News section is working properly and api is fetching its data well.

--- src/components/News.jsx
+++ src/components/News.jsx
@@ -1,6 +1,62 @@
 import React from "react";
+import { Select, Row, Col, Card, Typography, Avatar } from "antd";
+import { useGetCryptoNewsQuery } from "../services/cryptoNewsApi";
+// import { momentjs } from "momentjs";
+const { Text, Title } = Typography;
+const { Option } = Select;
 
-const News = () => {
-  return <div className="home-page"></div>;
+const News = ({ simplified = true }) => {
+  const demoImage = "https://picsum.photos/200/300";
+  const { data: cryptoNews } = useGetCryptoNewsQuery({
+    newsCategory: "cryptocurrency",
+    count: simplified ? 6 : 12
+  });
+  console.log(cryptoNews);
+  if (!cryptoNews?.value)
+    return <div className="home-page"> Well we are loading your news</div>;
+
+  return (
+    <>
+      <Row gutter={[24, 24]}>
+        {cryptoNews.value.map((news, i) => (
+          <Col xs={24} sm={12} lg={8} key={i}>
+            <Card hoverable className="news-card">
+              <a href={news.url} target="_blank" rel="norereffer">
+                <div className="news-image-container">
+                  <Title className="news-title" level={4}>
+                    {news.name}
+                  </Title>
+                  <img
+                    src={news?.image?.thumbnail?.contentUrl || demoImage}
+                    alt="news"
+                  ></img>
+                </div>
+                <p>
+                  {news.description.length > 100
+                    ? `${news.description.substring(0, 100)}...`
+                    : news.description}
+                </p>
+
+                <div className="provider-container">
+                  <div>
+                    <Avatar
+                      src={
+                        news.provider[0]?.image?.thumbnail?.contentUrl ||
+                        demoImage
+                      }
+                    />
+                    <Text>
+                      {/* {momentjs(news.datePublished).startOf("ss").fromNow()} */}
+                    </Text>
+                  </div>
+                </div>
+              </a>
+            </Card>
+          </Col>
+        ))}
+      </Row>
+    </>
+  );
 };
+
 export default News;
